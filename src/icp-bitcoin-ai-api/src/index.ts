6c4aab6ee1d9cfa5158606eb0ff8d854b40708aa
felipesaadi <felipesaadisiegert@gmail.com>
2024-07-12 19:19:52 -0300

    feat: add get transaction

--- src/icp-bitcoin-ai-api/src/index.ts
+++ src/icp-bitcoin-ai-api/src/index.ts
@@ -1,8 +1,10 @@
 import { Canister, query } from 'azle';
 import { HttpResponse, HttpTransformArgs } from 'azle/canisters/management';
+import { hashblock } from './hashblock';
 import { transaction } from './transaction';
 
 export default Canister({
+    ...hashblock,
     ...transaction,
     transform: query([HttpTransformArgs], HttpResponse, (args) => {
         return {
