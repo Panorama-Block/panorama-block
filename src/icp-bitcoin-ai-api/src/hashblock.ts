5ee26e8a2482d38f4249ed8278fd027c9b2e6e14
felipesaadi <felipesaadisiegert@gmail.com>
2024-07-13 22:10:16 -0300

    feat: add hashblocks and stable memory

--- src/icp-bitcoin-ai-api/src/hashblock.ts
+++ src/icp-bitcoin-ai-api/src/hashblock.ts
@@ -1,4 +1,4 @@
-import { ic, None, Principal, Some, text, int, update, jsonStringify, jsonParse } from "azle";
+import { ic, None, Principal, Some, text, int, update, jsonStringify, jsonParse, Record, nat, StableBTreeMap, nat8, nat64, float64, query, Vec, Opt } from "azle";
 import { managementCanister } from "azle/canisters/management";
 
 const defaultArgs = {
@@ -17,30 +17,75 @@ const defaultArgs = {
   })
 }
 
+const Hashblocks = Record({
+  id: text,
+  height: float64,
+  version: float64,
+  timestamp: float64,
+  tx_count: float64,
+  size: float64,
+  weight: float64,
+  merkle_root: text,
+  previousblockhash: text,
+  mediantime: float64,
+  nonce: float64,
+  bits: float64,
+  difficulty: float64
+});
+type Hashblocks = typeof Hashblocks.tsType;
+
+let hashblocksMap = StableBTreeMap<string, Hashblocks>(2);
+
 export const hashblock = {
-  getHashblocks: update([text, int], text, async (id: string, count: int) => {
-    let lastId: string = id
-    const hashblocks = []
+  updateHashblocks: update([Opt(text), int], text, async (id: Opt<text>, count: int) => {
+    let lastId: string = id.Some?.toString() ?? hashblocksMap.keys().slice(-1)[0]
 
-    for (let i = 0; i < count; i++) {
-      const response = await ic.call(
-        managementCanister.http_request,
-        {
-          args: [
-            {
-              url: `https://api.mempool.space/api/block/${lastId}`,
-              ...defaultArgs
-            }
-          ],
-          cycles: 50_000_000n
-        }
-      );
-
-      const data: any = Buffer.from(response.body).toString()
-      hashblocks.push(data)
-      lastId = jsonParse(data).previousblockhash
+    if (!lastId) {
+      return 'No Hashblocks found'
     }
 
-    return JSON.stringify(hashblocks)
-  })
+    for (let i = 0; i < count; i++) {
+      const hasHashblock = hashblocksMap.containsKey(lastId)
+
+      if (!hasHashblock) {
+        const response = await ic.call(
+          managementCanister.http_request,
+          {
+            args: [
+              {
+                url: `https://api.mempool.space/api/block/${lastId}`,
+                ...defaultArgs
+              }
+            ],
+            cycles: 5_000_000n
+          }
+        );
+
+        const data: any = Buffer.from(response.body).toString()
+        const json: Hashblocks = jsonParse(data)
+        hashblocksMap.insert(lastId, json)
+        lastId = json.previousblockhash
+      }
+      else {
+        lastId = hashblocksMap.get(lastId).Some!.previousblockhash
+      }
+    }
+
+    return jsonStringify(hashblocksMap.keys())
+  }),
+  removeHashblocks: update([], text, () => {
+    let hashblocks = hashblocksMap.keys()
+
+    hashblocks.forEach((id) => {
+      hashblocksMap.remove(id)
+    })
+
+    return 'Wiped All Hashblocks'
+  }),
+  getHashblockIds: query([], Vec(text), () => {
+    return hashblocksMap.keys();
+  }),
+  getHashblocks: query([], Vec(Hashblocks), () => {
+    return hashblocksMap.values();
+  }),
 }
