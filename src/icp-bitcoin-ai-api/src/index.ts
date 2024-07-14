eb5274eed4b699ba7d65144f7e7ab4370b994a77
felipesaadi <felipesaadisiegert@gmail.com>
2024-07-14 19:13:23 -0300

    feat: add whale and address functions

--- src/icp-bitcoin-ai-api/src/index.ts
+++ src/icp-bitcoin-ai-api/src/index.ts
@@ -2,10 +2,12 @@ import { Canister, query } from 'azle';
 import { HttpResponse, HttpTransformArgs } from 'azle/canisters/management';
 import { hashblock } from './hashblock';
 import { transaction } from './transaction';
+import { address } from './address';
 
 export default Canister({
     ...hashblock,
     ...transaction,
+    ...address,
     transform: query([HttpTransformArgs], HttpResponse, (args) => {
         return {
             ...args.response,
