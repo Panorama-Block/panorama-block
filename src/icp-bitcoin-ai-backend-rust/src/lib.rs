9b9910d2c01057d41ef934c834802f3c9b2553e0
misterclayt0n <daviarantes13@gmail.com>
2024-07-16 16:32:42 -0300

    feat: add get_hash_block function (mempool) Co-authored-by: c0utin <rafaelcouto111@gmail.com>

--- src/icp-bitcoin-ai-backend-rust/src/lib.rs
+++ src/icp-bitcoin-ai-backend-rust/src/lib.rs
@@ -1,4 +1,38 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use ic_cdk::api::management_canister::http_request::{
+    http_request, CanisterHttpRequestArgument, HttpHeader, HttpMethod,
+};
+
+#[ic_cdk::update]
+async fn get_hash_block(hash_block: String) -> String {
+    let url = format!("https://mempool.space/api/block/{hash_block}/txs");
+
+    let request_headers = vec![
+        HttpHeader {
+            name: "User-Agent".to_string(),
+            value: "https://panoramablock.com".to_string(),
+        },
+    ];
+
+    let request = CanisterHttpRequestArgument {
+        url: url.to_string(),
+        method: HttpMethod::GET,
+        body: None,
+        max_response_bytes: None,
+        transform: None,
+        headers: request_headers,
+    };
+
+    match http_request(request, 1603126400).await {
+        Ok((response,)) => {
+            let str_body = String::from_utf8(response.body)
+                .expect("Transformed response is not UTF-8 encoded.");
+            str_body
+        }
+
+        Err((r, m)) => {
+            let message =
+                format!("The http_request resulted into error. RejectionCode: {r:?}, Error: {m}");
+            message
+        }
+    }
 }
