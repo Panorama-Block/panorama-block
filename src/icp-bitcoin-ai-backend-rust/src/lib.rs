e7a003b1d91b66caa6ad3d10263cb4acd7fd1220
misterclayt0n <daviarantes13@gmail.com>
2024-07-17 16:57:32 -0300

    feat: add set_hashblock with state manipulation
    Co-authored-by: c0utin <rafaelcouto111@gmail.com>

--- src/icp-bitcoin-ai-backend-rust/src/lib.rs
+++ src/icp-bitcoin-ai-backend-rust/src/lib.rs
@@ -1,18 +1,64 @@
-<<<<<<< HEAD
+//use candid::CandidType;
 use ic_cdk::api::management_canister::http_request::{
     http_request, CanisterHttpRequestArgument, HttpHeader, HttpMethod,
 };
 
+use candid::{CandidType, Deserialize};
+use ic_stable_memory::collections::SVec;
+use ic_stable_memory::derive::StableType;
+use ic_stable_memory::SBox;
+use std::cell::RefCell;
+use std::panic::catch_unwind;
+
+#[derive(CandidType, Deserialize, StableType, Debug, Clone)]
+struct Hashblock {
+    id: String,
+    height: f64,
+    version: f64,
+    timestamp: f64,
+    tx_count: f64,
+    size: f64,
+    weight: f64,
+    merkle_root: String,
+    previousblockhash: String,
+    mediantime: f64,
+    nonce: f64,
+    bits: f64,
+    difficulty: f64,
+}
+
+type __Hashblocks = SVec<SBox<Hashblock>>;
+
+thread_local! {
+    static CURRENT_HASHBLOCK: RefCell<CurrentHashblock> = RefCell::new(CurrentHashblock::new(String::new()));
+}
+
+struct CurrentHashblock {
+    hash: String,
+}
+
+impl CurrentHashblock {
+    pub fn new(hash: String) -> Self {
+        Self { hash }
+    }
+
+    pub fn update(&mut self, new_hash: String) {
+        self.hash = new_hash;
+    }
+
+    pub fn get_hash(&self) -> &String {
+        &self.hash
+    }
+}
+
 #[ic_cdk::update]
-async fn get_hash_block(hash_block: String) -> String {
+async fn get_hashblock(hash_block: String) -> String {
     let url = format!("https://mempool.space/api/block/{hash_block}/txs");
 
-    let request_headers = vec![
-        HttpHeader {
-            name: "User-Agent".to_string(),
-            value: "https://panoramablock.com".to_string(),
-        },
-    ];
+    let request_headers = vec![HttpHeader {
+        name: "User-Agent".to_string(),
+        value: "https://panoramablock.com".to_string(),
+    }];
 
     let request = CanisterHttpRequestArgument {
         url: url.to_string(),
@@ -36,9 +82,29 @@ async fn get_hash_block(hash_block: String) -> String {
             message
         }
     }
-=======
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
->>>>>>> 06585f8 (rebase from develop)
 }
+
+#[ic_cdk::update]
+async fn set_hashblock(hash_block: String) -> Result<String, String> {
+    let result = catch_unwind(|| {
+        CURRENT_HASHBLOCK.with(|current| {
+            current.borrow_mut().update(hash_block);
+        });
+    });
+
+    match result {
+        Ok(_) => Ok("Hash block was set successfully".to_string()),
+        Err(_) => Err("Err to set Hash block".to_string()),
+    }
+}
+
+#[ic_cdk::update]
+async fn get_set_hash_block() -> String {
+    CURRENT_HASHBLOCK.with(|current| {
+        current.borrow().get_hash().clone()
+    })
+}
+
+// setHashblock: update([text], Void, (id) => {
+//   currentHashblock = id;
+// }),
